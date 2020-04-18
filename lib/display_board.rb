c594fb25cc9d8c78e22d0587cc641be788560a8a
Samir Triande <habib.triande@gmail.com>
2020-04-18 19:15:57 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,10 +6,4 @@ def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
   puts "   |   |   "
 end
 
-def display_board(board = [" ", " ", " ", " ", "X", " ", " ", " ", " "])
-  puts "   |   |   "
-  puts "-----------"
-  puts "   | X |   "
-  puts "-----------"
-  puts "   |   |   "
-end
+display_board
